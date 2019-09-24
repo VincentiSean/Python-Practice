b7aadccd9d3782eca2141aaf934ae3f844ae9b4f
Sean Vincenti <sjv18@pitt.edu>
2019-09-24 00:29:50 -0400

    Add files via upload
    So many changes. Redid the entire file with beautifulsoup4 instead of opening the browser with selenium. Much easier to go into the network --> find a link to a page of each set of results --> then download the images from there rather than get the js to load to get all results. Downloader is set to take the first 10 pages of results.

--- imgurDownloader.py
+++ imgurDownloader.py
@@ -2,22 +2,45 @@
 # imgurDownloader.py - This project takes an argument and searches imgur with the user input
 #   and proceeds to download all image search results.
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import bs4, sys, requests, os
 
 # Get user input
-print('Please enter a tag to  search & download from Imgur.')
-tag = input()
+tag = sys.argv[1]
 
-# Assign Firefox as the browser of choice
-browser = webdriver.Firefox()
-browser.get('https://imgur.com')
-print('Connecting to imgur...')
+# Make a directory for the images to be stored
+directory = 'imgurSearch(' + tag + ')'
+os.makedirs(directory, exist_ok=True)
 
-# Search imgur with user input 
-print('Searching for the images...')
-searchbar = browser.find_element_by_xpath('//input[@class="Searchbar-textInput"]')
-searchbar.send_keys(tag)
-searchbar.send_keys(Keys.ENTER)
+# Connect to imgur with tag from arguement and search
+for x in range(10):
+    website = 'https://imgur.com/search/score/all/page/' + str(x+1) + '?scrolled&q=' + tag + '&'
+    try:
+        res = requests.get(website)
+            # print(website)
+        print('Connecting to imgur...')
+        res.raise_for_status()
 
-# Loop through all images and download
+
+        # Get the soup to find the image links from search results
+        soupy = bs4.BeautifulSoup(res.text, 'lxml')
+            # print(soupy)
+        imgElem = soupy.select('img')
+
+        for i in range(len(imgElem)):  
+            imgSrc = 'https:' + imgElem[i].get('src')
+            # print(imgSrc)
+
+            # Download the image     
+            res = requests.get(imgSrc)
+            res.raise_for_status()
+            
+            # Save image to folder
+            imageFile = open(os.path.join(directory,  os.path.basename(imgSrc)), 'wb')
+            for chunk in res.iter_content(100000):
+                imageFile.write(chunk)
+            imageFile.close()
+
+    except Exception as exc:
+        print('There was a problem: %s' % (exc))
+
+print('Could not find any more images.')
